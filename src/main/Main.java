7d5bc997f856fba63bad3d7d280c229aaf08b003
Euller <eullernobrega@gmail.com>
2020-07-28 01:42:51 -0300

    Usando default methods em foreach com Lambda

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,25 @@
 package main;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Main {
 
 	public static void main(String[] args) {
-		 final int numero = 1; //  Lambda encontra variasveis de valores imutaveis
-		 int numero2 = 2; // tbm eh possivel ser encontrado nessa forma caso nao haja modificao da variavel depois
+		Usuario user1 = new Usuario("Paulo Silveira", 150);
+		Usuario user2 = new Usuario("Rodrigo Turini", 120);
+		Usuario user3 = new Usuario("Guilherme Silveira", 190);
+		List<Usuario> usuarios = Arrays.asList(user1, user2, user3);
+		Consumer<Usuario> mostraMensagem = u -> System.out.println("antes de imprimir os nomes");
+		Consumer<Usuario> imprimeNome = u -> System.out.println(u.getNome());
+		usuarios.forEach(mostraMensagem.andThen(imprimeNome)); // As interfaces consideradas @FunctionalInterface além
+																// de terem somente um método abstrato
+																// Agora também default métodos os quais podem ser
+																// chamados caso n haja nenhma implementação ou incluido
+																// com o método
+																// Inferido pela Lambda function
 		
-		
-		Runnable runnable = () -> System.out.println(numero + numero2);
-		new Thread(runnable).start();
 	}
-		
+
 }
